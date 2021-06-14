eb8766d363abc431f77741bce20ed054d6b735b5
Matt Caswell <matt.caswell@hotmail.com>
2021-06-14 23:01:05 +0100

    added imgs to buttons

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-    alert("I got clicked!");
-  });
+      this.style.color="white"
+  }
+  )
 }
+
+
+// var audio = new Audio ('sounds/tom-1.mp3')
+// audio.play();
